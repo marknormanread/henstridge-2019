bf716b92cda6844692b9d61d71ece07aa7ea95ef
Mark N. Read <>
2019-07-11 11:21:29 +1000

    Specify colours per group

---
+++ commons.R
@@ -0,0 +1,15 @@
+#### Colours used for graphing
+col_per_group = c(
+  'HF' = '#C34A36',
+  'NC' = '#4B4453',
+  
+  # Simple toxicity (severe vs non-severe)
+  'Sed' = '#AF5C00',  # light grey. Alternatively, for dark grey, #7A7485
+  'Ex' = '#008E83',
+  
+  # Full toxicity (three levels)
+  'NC_Sed' = '#d98ec5',
+  'NC_Ex' = '#26b3c5',
+  'HF_Sed' = '#7ebb2e',
+  'HF_Ex' = '#f78c80'
+)
