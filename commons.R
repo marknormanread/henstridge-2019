cee09ce189ca9f6bde8d1b1d68790fb4cce32c40
Mark N. Read <>
2019-07-11 22:03:11 +1000

    Better colours

--- commons.R
+++ commons.R
@@ -1,15 +1,29 @@
 #### Colours used for graphing
 col_per_group = c(
-  'HF' = '#C34A36',
-  'NC' = '#4B4453',
+  'NC' = '#1F618D',
+  'HF' = '#922B21',
   
   # Simple toxicity (severe vs non-severe)
-  'Sed' = '#AF5C00',  # light grey. Alternatively, for dark grey, #7A7485
-  'Ex' = '#008E83',
+  'Sed' = '#A6ACAF',
+  'Ex' = '#34495E',
   
-  # Full toxicity (three levels)
-  'NC_Sed' = '#D190C4',
-  'NC_Ex' = '#4BB0C4',
-  'HF_Sed' = '#88BB39',
-  'HF_Ex' = '#ED9181'
+  # # Full toxicity (three levels)
+  'NC_Sed' = '#85C1E9',
+  'NC_Ex' = '#1F618D',
+  'HF_Sed' = '#F1948A',
+  'HF_Ex' = '#922B21'
+  
+  
+  # 'HF' = '#C34A36',
+  # 'NC' = '#4B4453',
+  # 
+  # # Simple toxicity (severe vs non-severe)
+  # 'Sed' = '#F4511E',
+  # 'Ex' = '#1E88E5',
+  
+  
+  # 'NC_Sed' = '#d647b9',
+  # 'NC_Ex' = '#06a5c4',
+  # 'HF_Sed' = '#88BB39',
+  # 'HF_Ex' = '#ED9181' 
 )
